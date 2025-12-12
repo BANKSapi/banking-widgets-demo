112ae36f33c02f6d0c39f4e365d4a5f37deaf0c8
Niklas Callsen <niklas.callsen@banksapi.de>
2025-12-12 08:13:38 +0100

    adjust eslint rules

--- capacitor-vue-app/eslint.config.ts
+++ capacitor-vue-app/eslint.config.ts
@@ -21,6 +21,13 @@ export default defineConfigWithVueTs(
   vueTsConfigs.recommended,
 
   {
+    name: 'app/rules',
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+
+  {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
